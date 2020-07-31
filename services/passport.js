863186966e919f8987354bc5f716ccb251f6ae68
areddin409 <areddin409@gmail.com>
2020-07-31 15:57:49 -0500

    x

--- services/passport.js
+++ services/passport.js
@@ -12,15 +12,6 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
     },
-    async (accessToken, refreshToken, profile, done) => {
-      const exitsingUser = await User.findOne({ googleId: profile.id });
-
-      if (exitsingUser) {
-        return done(null, exitsingUser);
-      }
-
-      const user = await new User({ googleId: profile.id }).save();
-      done(null, user);
-    }
+    (accessToken, refreshToken, profile, done) => {}
   )
 );
