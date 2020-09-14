898cc99ba8f9c9ff496555de30b342b52adc7082
areddin409 <areddin409@gmail.com>
2020-09-14 09:47:13 -0500

    fixed webhook issue

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,5 @@
 const _ = require('lodash');
-const Path = require('path-parser');
+const { Path } = require('path-parser');
 const { URL } = require('url');
 
 const mongoose = require('mongoose');
@@ -25,7 +25,7 @@ module.exports = (app) => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const p = new Path('/api/surveys/:surveyId/:choice');
+    const p = new Path.createPath('/api/surveys/:surveyId/:choice');
 
     _.chain(req.body)
       .map(({ email, url }) => {
